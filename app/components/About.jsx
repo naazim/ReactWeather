6b79a089c2906ded8164e260226f0fa66aefb4bc
naazim <naazim4u@gmail.com>
2016-12-19 00:22:01 +0100

    About issue fixed

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,16 +1,12 @@
 var React = require('react');
 
-var About = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <h3>About</h3>
-        <p>Welcome to About page.</p>
-      </div>
-    )
-  }
-});
-
-var About = (props) => <h3>About Component</h3>;
+var About = (props) => {
+  return (
+    <div>
+      <h3>About</h3>
+      <p>Welcome to About page.</p>
+    </div>
+  )
+}
 
 module.exports = About;
